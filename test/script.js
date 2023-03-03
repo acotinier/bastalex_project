43ca9f91525f8e472cf22ed781fa27f6d3637c79
acotinier <archopliba031.prui@gmail.com>
2023-03-03 10:14:27 +0100

    add animation
    add animation on label color switch

--- test/script.js
+++ test/script.js
@@ -6,10 +6,14 @@ function flipToConn(){
         alert("Vous êtes déjà sur l'onglet de connexion.");
     }
     else{
+        const labelinscr = document.querySelector("#labelinscr");
+        const labelconn = document.querySelector("#labelconn");
         const card = document.querySelector("#flip-box-inner");
         card.classList.add("flipboxtofront");
         card.classList.remove("flipingboxback");
         flipboxState = 0;
+        labelinscr.classList.add("nonactiveflip");
+        labelconn.classList.remove("nonactiveflip");
         console.log("flipToConn success -> " , flipboxState);
     }
 }
@@ -18,10 +22,14 @@ function flipToInscr(){
         alert("Vous êtes déjà sur l'onglet d'inscription.");
     }
     else{
+        const labelinscr = document.querySelector("#labelinscr");
+        const labelconn = document.querySelector("#labelconn");
         const card = document.querySelector("#flip-box-inner");
         card.classList.remove("flipboxtofront");
         card.classList.add("flipingboxback");
         flipboxState = 1;
+        labelconn.classList.add("nonactiveflip");
+        labelinscr.classList.remove("nonactiveflip");
         console.log("flipToInscr success -> " , flipboxState);
     }
 }
